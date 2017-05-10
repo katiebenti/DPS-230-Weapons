80b4ceddf1a72054514029e2211c4bb06c361cae
katiebenti <kbentivegna@uchc.edu>
2017-05-10 09:04:15 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ln03oT5GL1caVmoCdbveOrw48n1i7UpxeJwL5I4VRmU/edit#gid=745832354';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ln03oT5GL1caVmoCdbveOrw48n1i7UpxeJwL5I4VRmU/pubhtml';
